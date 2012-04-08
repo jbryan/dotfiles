4d5cde90f2c81a8c9447ad81ff3e5745c479dcf0
Josh Bryan <jbryan@ci.uchicago.edu>
2012-04-08 15:14:10 -0500

    Added screen and command-t to installer.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+pushd .vim/bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
+popd
+
 ln -sf $PWD/.vim ~/.vim
 ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.screenrc ~/.screenrc
+
+
