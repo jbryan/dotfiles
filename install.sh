511ade10c98b378d66199555d7e48401853bc61a
Josh Bryan <jbryan@ci.uchicago.edu>
2012-04-09 08:53:08 -0500

    Fixed install to remove command-t

--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ cd $(dirname $0)
 git submodule init
 git submodule update
 
-pushd vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
-popd
-
 DOTFILES="vim vimrc screenrc bashrc bash_logout profile"
 
 for file in $DOTFILES; do
