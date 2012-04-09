3126dad439739a7b62188d22614c40e9205dd263
Josh Bryan <jbryan@ci.uchicago.edu>
2012-04-09 11:07:28 -0500

    added irbrc

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
+#Config
+
+DOTFILES="
+	vim 
+	vimrc 
+	screenrc 
+	bashrc 
+	bash_logout 
+	profile 
+	irbrc
+"
+
+
+# Do the install
 cd $(dirname $0)
 
 git submodule init
 git submodule update
 
-DOTFILES="vim vimrc screenrc bashrc bash_logout profile"
-
 for file in $DOTFILES; do
+	echo "Linking $file"
 	ln -sfT $PWD/$file ~/.$file
 done
 
